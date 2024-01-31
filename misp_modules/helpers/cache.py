606bb08541ff11950a275875919addc16ec4db04
glonfandango <glyn@chudleigh.io>
2024-01-31 21:52:30 +0000

    fix: allow redis password to be retrieved by environment variable

--- misp_modules/helpers/cache.py
+++ misp_modules/helpers/cache.py
@@ -24,15 +24,21 @@ import os
 import redis
 import hashlib
 
-port = int(os.getenv("REDIS_PORT")) if os.getenv("REDIS_PORT") else 6379
-hostname = os.getenv("REDIS_BACKEND") or '127.0.0.1'
-db = int(os.getenv("REDIS_DATABASE")) if os.getenv("REDIS_DATABASE") else 0
+port = int(os.getenv("REDIS_PORT"), 6379)
+hostname = os.getenv("REDIS_BACKEND", '127.0.0.1')
+db = int(os.getenv("REDIS_DATABASE"), 0)
+password = os.getenv("REDIS_PW")
 
+# Check if REDIS_PW is a path to a file
+# For use with mounted secrets
+if password and os.path.isfile(password):
+    with open(password, 'r') as file:
+        password = file.read().strip()
 
 def selftest(enable=True):
     if not enable:
         return False
-    r = redis.Redis(host=hostname, port=port, db=db)
+    r = redis.Redis(host=hostname, port=port, db=db, password=password)
     try:
         r.ping()
     except Exception:
@@ -42,7 +48,7 @@ def selftest(enable=True):
 def get(modulename=None, query=None, value=None, debug=False):
     if (modulename is None or query is None):
         return False
-    r = redis.Redis(host=hostname, port=port, db=db, decode_responses=True)
+    r = redis.Redis(host=hostname, port=port, db=db, password=password,decode_responses=True)
     h = hashlib.sha1()
     h.update(query.encode('UTF-8'))
     hv = h.hexdigest()
@@ -60,7 +66,7 @@ def get(modulename=None, query=None, value=None, debug=False):
 
 
 def flush():
-    r = redis.StrictRedis(host=hostname, port=port, db=db, decode_responses=True)
+    r = redis.StrictRedis(host=hostname, port=port, db=db, password=password, decode_responses=True)
     returncode = r.flushdb()
     return returncode
 
